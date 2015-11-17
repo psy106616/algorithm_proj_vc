c715986c1cec16e558ddeefdc9c31d656eabb6cc
psy106616 <psy106616@gmail.com>
2015-11-16 19:51:40 -0500

    add priority queue for nodes

--- TeamD_vc/src/Graph.java
+++ TeamD_vc/src/Graph.java
@@ -4,6 +4,11 @@ public class Graph {
 	int numNodes = 0;
 	int numEdges = 0;
 	HashMap<Integer, Set<Integer>> Nodes = new HashMap<Integer, Set<Integer>>();
+	PriorityQueue<int[]> mostConnectedNodes = new PriorityQueue<int[]>(){
+		public int compare(int[] N1, int[] N2){
+			return N2[1]-N1[1];
+		}
+	};
 	
 	public Graph(){
 		
@@ -60,5 +65,16 @@ public class Graph {
 	
 	public boolean checkEdgeVisited(){
 		return true;
+	}
+	
+	public void updateMostConnectedNodes(){
+		if(this.numNodes==0)
+			return;
+		for(Integer key: this.Nodes.keySet()){
+			int[] temp = new int[2];
+			temp[0] = key;
+			temp[1] = Nodes.get(key).size();
+			this.mostConnectedNodes.add(temp);
+		}
 	}
 }
